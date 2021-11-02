e8f3740f4beed4796252204e7dde6d65ff068143
Cecilie Svangtun Nowak <201910286@post.au.dk>
2021-11-02 16:47:28 +0100

    Add project files.

---
+++ GitHubTest1/Repos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitHubTest1
+{
+    class Repos
+    {
+        public int Counter { get; set; }
+        
+
+        public Repos()
+        {
+            Counter = 10;
+        }
+    }
+}
